c1cd695fdb97955c5c61237ef0d37edc00f8e55c
calimer <platinumarts@gmail.com>
2016-01-28 00:41:29 -0500

    Added some more tutorials to the mods doc. Having some fun with Mikesmod :)  Added a welcome message and bricks now speak if you hit them.

--- mods/mikesmod/init.lua
+++ mods/mikesmod/init.lua
@@ -4,3 +4,19 @@ minetest.register_craftitem("mikesmod:waste", {
 	groups={waste},
 })
 
+minetest.after(10,
+	function(params)
+		minetest.chat_send_all(" Welcome to the Modern City Builder Gametype.  Press I for your inventory!  Press escape to open the game menu.")
+	end
+)
+
+minetest.register_on_punchnode(
+	function(pos, node, puncher)
+		if node.name == "default:brick" then
+			for _,player in ipairs(minetest.get_connected_players()) do
+			local name = player:get_player_name()
+			minetest.chat_send_all("Hey " .. name .. ", why are you hitting me?")
+		end
+	end
+end	
+)
