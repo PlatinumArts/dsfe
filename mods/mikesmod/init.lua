89c139f6a9e68f249291b8f9c1ab8f8f95d253f0
calimer <platinumarts@gmail.com>
2016-01-31 22:04:43 -0500

    Hopefully code improvements to my mod :)

--- mods/mikesmod/init.lua
+++ mods/mikesmod/init.lua
@@ -1,22 +1,26 @@
 minetest.register_craftitem("mikesmod:waste", {
 	description = "Waste",
 	inventory_image = "mikesmod_waste.png",
-	groups={waste},
+	--groups={waste},
 })
 
+--[[
 minetest.after(10,
 	function(params)
 		minetest.chat_send_all(" Welcome to the Modern City Builder Gametype.  Press I for your inventory!  Press escape to open the game menu.")
 	end
 )
+--]]
+
+minetest.register_on_joinplayer(function(player)
+	minetest.chat_send_all("Welcome "..player:get_player_name().."! Press I for your inventory!  Press escape to open the game menu.")
+end)
 
 minetest.register_on_punchnode(
 	function(pos, node, puncher)
 		if node.name == "default:brick" then
-			for _,player in ipairs(minetest.get_connected_players()) do
-			local name = player:get_player_name()
+			local name = puncher:get_player_name()
 			minetest.chat_send_all("Hey " .. name .. ", why are you hitting me?")
 		end
-	end
-end	
+	end	
 )
