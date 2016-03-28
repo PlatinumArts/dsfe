7f8c9b6f68e1670873b016997d1dd4da2e23fba7
Mike <platinumarts@gmail.com>
2016-03-27 23:49:00 -0400

    small update

--- games/donmode/mods/walls/init.lua
+++ games/donmode/mods/walls/init.lua
@@ -7,9 +7,8 @@ Copyright (C) 2015 Auke Kok <sofar@foo-projects.org>
 
 This program is free software. It comes without any warranty, to
 the extent permitted by applicable law. You can redistribute it
-and/or modify it under the terms of the Do What The Fuck You Want
-To Public License, Version 2, as published by Sam Hocevar. See
-http://sam.zoy.org/wtfpl/COPYING for more details.
+and/or modify it under the terms of the CC0 License.
+https://creativecommons.org/publicdomain/zero/1.0/
 
 --]]
 
